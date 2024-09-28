c17bee83912e5be1168349ff7f762417b4f25f64
Hubert Z <hubcio992@gmail.com>
2024-09-28 17:50:24 +0200

    prototype level

--- Assets/Scripts/AbilityPl1.cs
+++ Assets/Scripts/AbilityPl1.cs
@@ -10,7 +10,7 @@ public class AbilityPl1 : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        rb.tag = "Player";
     }
 
     // Update is called once per frame
@@ -19,13 +19,14 @@ public class AbilityPl1 : MonoBehaviour
         if (Input.GetKeyDown("q"))
         {
             rb.transform.localScale = new UnityEngine.Vector3(3f, 3f, 0);
+            rb.tag = "StrongLemur";
 
         }
 
         if (Input.GetKeyDown("e"))
         {
             rb.transform.localScale = new UnityEngine.Vector3(1f, 1f, 0);
-
+            rb.tag = "Player";
         }
     }
 }
