084cbe47b214f50c93bad73486b0a5776678b1ac
Hubert Z <hubcio992@gmail.com>
2024-09-28 14:26:19 +0200

    Added: PlayerSwitch for 4 players

--- Assets/Scripts/PlayerSwitch.cs
+++ Assets/Scripts/PlayerSwitch.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class PlayerSwitch : MonoBehaviour
 {
 
-    [SerializeField] private bool isPlayer1On;
-    [SerializeField] private bool isPlayer2On;
+    //[SerializeField] private bool isPlayer1On;
+    //[SerializeField] private bool isPlayer2On;
 
-    [SerializeField] private PlayerController pc1;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private GameObject player3;
+    [SerializeField] private GameObject player4;
     // Start is called before the first frame update
     void Start()
     {
-        isPlayer1On = true;
-        isPlayer2On = false;
+        player1.GetComponent<PlayerController>().enabled = true;
+        player2.GetComponent<PlayerController>().enabled = false;
+        player3.GetComponent<PlayerController>().enabled = false;
+        player4.GetComponent<PlayerController>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("1")) {
+            player1.GetComponent<PlayerController>().enabled = true;
+            player2.GetComponent<PlayerController>().enabled = false;
+            player3.GetComponent<PlayerController>().enabled = false;
+            player4.GetComponent<PlayerController>().enabled = false;
+        }
+        if(Input.GetKeyDown("2")) {
+            player1.GetComponent<PlayerController>().enabled = false;
+            player2.GetComponent<PlayerController>().enabled = true;
+            player3.GetComponent<PlayerController>().enabled = false;
+            player4.GetComponent<PlayerController>().enabled = false;
+        }
+        if(Input.GetKeyDown("3")) {
+            player1.GetComponent<PlayerController>().enabled = false;
+            player2.GetComponent<PlayerController>().enabled = false;
+            player3.GetComponent<PlayerController>().enabled = true;
+            player4.GetComponent<PlayerController>().enabled = false;
+        }
+        if(Input.GetKeyDown("4")) {
+            player1.GetComponent<PlayerController>().enabled = false;
+            player2.GetComponent<PlayerController>().enabled = false;
+            player3.GetComponent<PlayerController>().enabled = false;
+            player4.GetComponent<PlayerController>().enabled = true;
+        }
     }
 }
