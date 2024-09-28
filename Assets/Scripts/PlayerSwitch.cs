a24e2bb00ee0a15a77f7f0c199b053e5a70ad8ae
Maciej Owczarzak <omaciejj@gmail.com>
2024-09-28 14:58:38 +0200

    feat: jumping on top of other players

--- Assets/Scripts/PlayerSwitch.cs
+++ Assets/Scripts/PlayerSwitch.cs
@@ -12,41 +12,53 @@ public class PlayerSwitch : MonoBehaviour
     [SerializeField] private GameObject player2;
     [SerializeField] private GameObject player3;
     [SerializeField] private GameObject player4;
+
+    private PlayerController pc1;
+    private PlayerController pc2;
+    private PlayerController pc3;
+    private PlayerController pc4;
+
     // Start is called before the first frame update
     void Start()
     {
-        player1.GetComponent<PlayerController>().enabled = true;
-        player2.GetComponent<PlayerController>().enabled = false;
-        player3.GetComponent<PlayerController>().enabled = false;
-        player4.GetComponent<PlayerController>().enabled = false;
+        pc1 = player1.GetComponent<PlayerController>();
+        pc2 = player2.GetComponent<PlayerController>();
+        pc3 = player3.GetComponent<PlayerController>();
+        pc4 = player4.GetComponent<PlayerController>();
+
+
+        pc1.enabled = true;
+        pc2.enabled = false;
+        pc3.enabled = false;
+        pc4.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("1")) {
-            player1.GetComponent<PlayerController>().enabled = true;
-            player2.GetComponent<PlayerController>().enabled = false;
-            player3.GetComponent<PlayerController>().enabled = false;
-            player4.GetComponent<PlayerController>().enabled = false;
+            pc1.enabled = true;
+            pc2.enabled = false;
+            pc3.enabled = false;
+            pc4.enabled = false;
         }
         if(Input.GetKeyDown("2")) {
-            player1.GetComponent<PlayerController>().enabled = false;
-            player2.GetComponent<PlayerController>().enabled = true;
-            player3.GetComponent<PlayerController>().enabled = false;
-            player4.GetComponent<PlayerController>().enabled = false;
+            pc1.enabled = false;
+            pc2.enabled = true;
+            pc3.enabled = false;
+            pc4.enabled = false;
         }
         if(Input.GetKeyDown("3")) {
-            player1.GetComponent<PlayerController>().enabled = false;
-            player2.GetComponent<PlayerController>().enabled = false;
-            player3.GetComponent<PlayerController>().enabled = true;
-            player4.GetComponent<PlayerController>().enabled = false;
+            pc1.enabled = false;
+            pc2.enabled = false;
+            pc3.enabled = true;
+            pc4.enabled = false;
         }
         if(Input.GetKeyDown("4")) {
-            player1.GetComponent<PlayerController>().enabled = false;
-            player2.GetComponent<PlayerController>().enabled = false;
-            player3.GetComponent<PlayerController>().enabled = false;
-            player4.GetComponent<PlayerController>().enabled = true;
+            pc1.enabled = false;
+            pc2.enabled = false;
+            pc3.enabled = false;
+            pc4.enabled = true;
         }
     }
 }
