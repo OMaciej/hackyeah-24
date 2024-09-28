b6cf8d6bc404435a47e2d448eff12542b4f4cac2
Hubert Z <hubcio992@gmail.com>
2024-09-28 16:28:33 +0200

    Added: StrongLemur tag, steelbox script

--- Assets/Scripts/SteelBoxScript.cs
+++ Assets/Scripts/SteelBoxScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,11 +8,23 @@ public class SteelBoxScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
 
+    [SerializeField] private float defaultMass = 5f;
+
     // Update is called once per frame
     void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.drag = 20;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) 
+    {
+        if(col.gameObject.tag != "StrongLemur") 
+        {
+            rb.mass = 10000;
+        } else 
+        {
+            rb.mass = defaultMass;
+        }
     }
     
 }
