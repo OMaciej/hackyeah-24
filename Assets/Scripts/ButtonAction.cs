50862c01053f2ac241123d8cc51523c897c2e875
Maciej Owczarzak <omaciejj@gmail.com>
2024-09-29 02:39:48 +0200

    feat: enable and disable functions for buttons

--- Assets/Scripts/ButtonAction.cs
+++ Assets/Scripts/ButtonAction.cs
@@ -2,10 +2,28 @@ using UnityEngine;
 
 public class ButtonAction : MonoBehaviour
 {
-    [SerializeField] private Action pressAction;
+    [SerializeField] private Action enableAction;
+    [SerializeField] private Action disableAction;
+
+    private bool toggled;
+
+    private void Start()
+    {
+        toggled = false;
+    }
 
     public void ClickButton()
     {
-        pressAction.PerformAction();
+        toggled = !toggled;
+
+        if (toggled)
+        {
+            enableAction.PerformAction();
+        } 
+        
+        else
+        {
+            disableAction.PerformAction();
+        }
     }
 }
