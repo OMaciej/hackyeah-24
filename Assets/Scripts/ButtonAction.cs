a439b7f7ba093b84ab49792587b4eb9fcb290e38
Maciej Owczarzak <omaciejj@gmail.com>
2024-09-29 02:55:30 +0200

    feat: textures for pressure plate

--- Assets/Scripts/ButtonAction.cs
+++ Assets/Scripts/ButtonAction.cs
@@ -5,6 +5,11 @@ public class ButtonAction : MonoBehaviour
     [SerializeField] private Action enableAction;
     [SerializeField] private Action disableAction;
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite onSprite;
+    [SerializeField] private Sprite offSprite;
+
+
     private bool toggled;
 
     private void Start()
@@ -18,11 +23,13 @@ public class ButtonAction : MonoBehaviour
 
         if (toggled)
         {
+            spriteRenderer.sprite = onSprite;
             enableAction.PerformAction();
         } 
         
         else
         {
+            spriteRenderer.sprite = offSprite;
             disableAction.PerformAction();
         }
     }
--- Assets/Scripts/PressurePlateAction.cs
+++ Assets/Scripts/PressurePlateAction.cs
@@ -4,32 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // This is very important if we want to restart the level
 public class PressurePlateAction : MonoBehaviour {
-  // Use this for initialization
-  void Start () {
-    
-  }
-  
-  // Update is called once per frame
-  void Update () {
-    
-  }
-
   public Action onEnterAction;
   public Action onExitAction;
 
-  bool isAllowedToPress(Collider2D other) {
-    return other.GetComponent<CanPressPressurePlate>() != null;
-  }
+  [SerializeField] private SpriteRenderer spriteRenderer;
+  [SerializeField] private Sprite onSprite;
+  [SerializeField] private Sprite offSprite;
+
+    private void Start()
+    {
+        spriteRenderer.sprite = offSprite;
+    }
+
+    bool isAllowedToPress(Collider2D other) {
+       return other.GetComponent<CanPressPressurePlate>() != null;
+    }
 
   // This function is called every time another collider overlaps the trigger collider
   void OnTriggerEnter2D (Collider2D other){
     if (isAllowedToPress(other)) {
+      spriteRenderer.sprite = onSprite;
       onEnterAction.PerformAction();
     }
   }
 
   void OnTriggerExit2D (Collider2D other) {
     if (isAllowedToPress(other)) {
+      spriteRenderer.sprite = offSprite;
       onExitAction.PerformAction();
     }
   }
