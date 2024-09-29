31d35c9ee16e1e282e192e199bf66d436cd2f164
Maciej Owczarzak <omaciejj@gmail.com>
2024-09-29 09:16:35 +0200

    whole bunch of shit

--- Assets/Scripts/AbilityPl1.cs
+++ Assets/Scripts/AbilityPl1.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class AbilityPl1 : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private bool toggled;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.tag = "Player";
+
+        toggled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("q"))
+        if(Input.GetKeyDown("q"))
         {
-            rb.transform.localScale = new UnityEngine.Vector3(2f, 2f, 0);
-            rb.tag = "StrongLemur";
+            toggled = !toggled;
 
-        }
+            if(toggled)
+            {
+                rb.transform.localScale = new UnityEngine.Vector3(2f, 2f, 0);
 
-        if (Input.GetKeyDown("e"))
-        {
-            rb.transform.localScale = new UnityEngine.Vector3(1f, 1f, 0);
-            rb.tag = "Player";
+                //makes the character able to push heavy objects
+                rb.tag = "StrongLemur";
+            } else
+            {
+                rb.transform.localScale = new UnityEngine.Vector3(1f, 1f, 0);
+                rb.tag = "Player";
+            }
         }
     }
 }
